75583f27de5d706b55d10322ba0b3ff898dba4c8
Siddhesh Gaykar <64803476+Siddhya007@users.noreply.github.com>
2020-07-15 14:08:08 +0530

    Updates

---
+++ precache-manifest.798b614a45018e25533179ad37997be1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee2cfe748121a8233b78ef8646f887c3",
+    "url": "/sid_resume/index.html"
+  },
+  {
+    "revision": "92425305341a922e4983",
+    "url": "/sid_resume/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "3c462145c9d5756bdf55",
+    "url": "/sid_resume/static/css/main.b1951e3c.chunk.css"
+  },
+  {
+    "revision": "92425305341a922e4983",
+    "url": "/sid_resume/static/js/2.faf15b46.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/sid_resume/static/js/2.faf15b46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c462145c9d5756bdf55",
+    "url": "/sid_resume/static/js/main.fdfd2400.chunk.js"
+  },
+  {
+    "revision": "8638020b3d2fffeba2d9",
+    "url": "/sid_resume/static/js/runtime-main.b9804399.js"
+  },
+  {
+    "revision": "6f94d8645e48068e7a19ba2550573818",
+    "url": "/sid_resume/static/media/1.6f94d864.jpg"
+  },
+  {
+    "revision": "119d8f7f1b83423e1a775029897a01b3",
+    "url": "/sid_resume/static/media/2.119d8f7f.jpg"
+  },
+  {
+    "revision": "932f92989e419fd1bb4ca8c1693c411d",
+    "url": "/sid_resume/static/media/3.932f9298.jpg"
+  },
+  {
+    "revision": "6349bd75123b901ab718919a6e0418f4",
+    "url": "/sid_resume/static/media/5.6349bd75.jpg"
+  },
+  {
+    "revision": "68838afd08a6a0293857b44c23c661b9",
+    "url": "/sid_resume/static/media/bg2.68838afd.jpg"
+  },
+  {
+    "revision": "eb05c4653525eb552dbc96c097abbba5",
+    "url": "/sid_resume/static/media/s.eb05c465.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1dbda3488649581fce3ed6b486e78a04.js"
+  "/sid_resume/precache-manifest.798b614a45018e25533179ad37997be1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sid_resume/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
